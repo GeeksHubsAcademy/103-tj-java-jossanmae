fee9e2360336ec76b5ad29c456a9bd93382082ad
smjcarlos96 <smjcarlos96@gmail.com>
2021-08-19 18:29:36 +0200

    Solution

--- src/main/java/es/geekshubs/academy/Arbol.java
+++ src/main/java/es/geekshubs/academy/Arbol.java
@@ -4,13 +4,26 @@ import java.util.Collections;
 
 public class Arbol {
 
-  /**
-   * Apply Method
-   * @param input
-   * @return
-   */
-  public String apply(final Integer input) {
-      return null;
-  }
+	/**
+	 * Apply Method
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public String apply(final Integer input) {
+		String output = "";
+		
+		for(int y=1; y<=input.intValue(); y++) {
+			for(int x=1; x<=input.intValue(); x++) {
+				if(x<y)
+					output += " ";
+				else
+					output += "*";
+			}
+			output += "\n";
+		}
+		
+		return output;
+	}
 
 }
